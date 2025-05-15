ff7b7b21cfa4c2da068d1d3b277e883ca6a1ef2c
moo <moogoesmeow123@gmail.com>
2025-05-14 19:35:47 -0700

    add test for v2

---
+++ test/_app/immutable/chunks/SdB0-QZi.js
@@ -0,0 +1,1 @@
+import{e as o,d as n,i as c}from"./CIEJGkll.js";import{l}from"./DIeogL5L.js";function u(t){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function f(t){n===null&&u(),l&&n.l!==null?i(n).m.push(t):o(()=>{const e=c(t);if(typeof e=="function")return e})}function i(t){var e=t.l;return e.u??(e.u={a:[],b:[],m:[]})}export{f as o};
